7b0876fda6b4251db4739bbaab01d0c424647db0
uranuswolf <158137715+uranuswolf@users.noreply.github.com>
2025-02-24 20:33:57 +0800

    Update motorControl.c

--- motors/motorControl.c
+++ motors/motorControl.c
@@ -1,9 +1,15 @@
-#include "motorControl.h"  // Include the header file for motor control
+#include "MotorControl.h"
+#include "PIDControl.h"
 
-// motorControl.c - Function to control the motors
+// Global variable for maximum motor speed (ticks per second)
+int maxMotorSpeed = 3000; // Example value, adjust based on your motor specifications
 
-void controlMotors(int leftSpeed, int rightSpeed)
-{
-    motor[leftmotor] = leftSpeed;
-    motor[rightmotor] = rightSpeed;
+void controlMotors(int leftPower, int rightPower) {
+    motor[motorL] = leftPower;
+    motor[motorR] = rightPower;
+}
+
+int getEncoderSpeed(int previousEncoder, int currentEncoder, int deltaTime) {
+    int deltaTicks = currentEncoder - previousEncoder;
+    return deltaTicks * 1000 / deltaTime;  // Calculate speed in ticks per second (scaled to milliseconds)
 }
