7cf1e75db7df6d13aa9aa31cc85e0e8ce1d7f372
uranuswolf <158137715+uranuswolf@users.noreply.github.com>
2025-02-24 15:48:00 +0800

    Create motorControl.h

---
+++ motors/motorControl.h
@@ -0,0 +1,8 @@
+// motorControl.h - Header file for motor control
+
+#ifndef MOTORCONTROL_H
+#define MOTORCONTROL_H
+
+void controlMotors(int leftSpeed, int rightSpeed);  // Declaration of the controlMotors function
+
+#endif
