4beef919e889d97f6f0e517b0537c224987b9bbe
uranuswolf <roychangapple@gmail.com>
2025-03-16 13:17:30 +0800

    Delete all

--- motors/motorControl.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOTORCONTROL_H
-#define MOTORCONTROL_H
-
-extern int maxMotorSpeed; // Maximum speed in encoder ticks per second
-
-void controlMotors(int leftPower, int rightPower);
-int getEncoderSpeed(int previousEncoder, int currentEncoder, int deltaTime);
-
-#endif
