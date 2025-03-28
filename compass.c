c0dde168268bc8e141e335015cfcd1ec51d780d1
yshelp123 <chongyushun@gmail.com>
2025-03-28 12:19:57 +0800

    Update compass.c

--- compass.c
+++ compass.c
@@ -35,6 +35,60 @@ task check_current_heading(){
 	}
 }
 
+// Move forward or backward by distance (in meters)
+void moveDistance(float distance, bool backward = false) {
+	float realDistance = distance *10; 
+    int targetTicks = distanceToTicks(realDistance);
+    SensorValue[LEFT_ENCODER] = 0;
+    SensorValue[RIGHT_ENCODER] = 0;
+
+    if (backward) {
+        motor[motorLeft] = basePower;
+        motor[motorRight] = -basePower;
+    } else {
+        motor[motorLeft] = -basePower;
+        motor[motorRight] = basePower;
+    }
+
+    while (abs(SensorValue[LEFT_ENCODER]) < targetTicks && abs(SensorValue[RIGHT_ENCODER]) < targetTicks) {
+        wait1Msec(10);
+    }
+
+    motor[motorLeft] = 0;
+    motor[motorRight] = 0;
+}
+
+// Turn left or right by angle (in degrees)
+void turnDegrees(float degrees, bool right = false) {
+    //float realDegrees = degrees * 2.5;
+    //float turningCircumference = 3.14159 * wheelBase;  // Full turning circle
+    //float arcLength = (realDegrees / 180.0) * turningCircumference; // One wheel arc distance
+
+    //Arc length formula : ArcLength=r*theta, where r is in radians 
+    float angleRadian = degreesToRadians(degrees); 
+    float arcLength = angleRadian * (wheelBase/2);
+	
+    int targetTicks = distanceToTicks(arcLength);
+
+    SensorValue[LEFT_ENCODER] = 0;
+    SensorValue[RIGHT_ENCODER] = 0;
+
+    if (right) {
+        motor[motorLeft] = -127;
+        motor[motorRight] = -127;
+    } else {
+        motor[motorLeft] = 127;
+        motor[motorRight] =127;
+    }
+
+    while (abs(SensorValue[LEFT_ENCODER]) < targetTicks && abs(SensorValue[RIGHT_ENCODER]) < targetTicks) {
+        wait1Msec(10);
+    }
+
+    motor[motorLeft] = 0;
+    motor[motorRight] = 0;
+}
+
 void backBase() {
     float heading = compass();  // Get current compass heading
     float degree;
